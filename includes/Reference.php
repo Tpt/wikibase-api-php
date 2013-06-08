6e57530617fabd0fdd6d3a88a6b25ca5f277ba51
Benestar <benestar.wikimedia@googlemail.com>
2013-06-08 20:27:49 +0300

    m

--- includes/Reference.php
+++ includes/Reference.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
-* A statement.
+* A reference.
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
